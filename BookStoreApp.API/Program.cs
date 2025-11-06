2a9e4dd3227eb066c4a6477c2c2a257b5c638fce
Paolo Paci <paolopci@yahoo.it>
2025-11-06 16:47:36 +0100

    install serilog and Seq

--- BookStoreApp.API/Program.cs
+++ BookStoreApp.API/Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +9,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Host. ()
+builder.Host.UseSerilog((ctx, lc) =>
+{
+    lc.WriteTo.Console();
+    lc.ReadFrom.Configuration(ctx.Configuration);
+});
 
 var app = builder.Build();
 
